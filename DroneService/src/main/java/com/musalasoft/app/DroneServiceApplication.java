f6f91374e46c8f3b33fb7a9fe19bbb342a65f7ed
nelson8013 <nelsonekpenyong@gmail.com>
2023-11-29 23:36:07 +0100

    created an empty spring boot application

---
+++ DroneService/src/main/java/com/musalasoft/app/DroneServiceApplication.java
@@ -0,0 +1,14 @@
+package com.musalasoft.app;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneServiceApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(DroneServiceApplication.class, args);
+    }
+}
